c8aae73f391faff3e1a49c94f13aa0f95f87b355
majdkassawat <majd.kassawat@gmail.com>
2020-02-20 22:56:24 +0100

    added 3 place rules

--- src/experiment/rules.lua
+++ src/experiment/rules.lua
@@ -1,9 +1,9 @@
 -- just as a reminder {black = 0, pink = 1, orange = 2, green = 3, blue = 4}
 local rules = {}
 rules.list = {
+    ----------------pickup rules--------------------------
+    -- considering seeing two blocks
     {
-        ----------------pickup rules--------------------------
-        -- considering seeing two blocks
         rule_type = 'pickup',
         structure = {
             {index = vector3(0, 0, 0), type = 2},
@@ -98,10 +98,50 @@ rules.list = {
             type = 1
         },
         generate_orientations = false
-    }
+    },
     --------------------------------------------------------
-    
-  
+    -------------------placing rules------------------------
+    {
+        rule_type = 'place',
+        structure = {
+            {index = vector3(0, 0, 0), type = 4}
+        },
+        light_axis = "X",
+        target = {
+            reference_index = vector3(0, 0, 0),
+            offset_from_reference = vector3(1, 0, 0),
+            type = 2
+        },
+        generate_orientations = false
+    },
+    {
+        rule_type = 'place',
+        structure = {
+            {index = vector3(0, 0, 0), type = 4},
+            {index = vector3(1, 0, 0), type = 2}
+        },
+        light_axis = "X",
+        target = {
+            reference_index = vector3(0, 0, 0),
+            offset_from_reference = vector3(1, 0, 0),
+            type = 2
+        },
+        generate_orientations = false
+    },
+    {
+        rule_type = 'place',
+        structure = {
+            {index = vector3(0, 0, 0), type = 2},
+            {index = vector3(1, 0, 0), type = 4}
+        },
+        light_axis = "X",
+        target = {
+            reference_index = vector3(0, 0, 0),
+            offset_from_reference = vector3(1, 0, 0),
+            type = 2
+        },
+        generate_orientations = false
+    }
 }
 rules.selection_method = 'nearest_win'
 return rules
